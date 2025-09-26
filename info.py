f681518494b5439c774d33190963e58d238c5669
sydneybloodworth <sydneybloodworth33@gmail.com>
2025-09-25 23:53:24 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,78 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpg"
+about_me = "I'm Sydney Bloodworth. I am a student at Georgia Tech aspiring to be in the medical field."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/sydney-bloodworth-0b3203354/"
+my_github_url = "https://github.com/sydneybloodworth"
+my_email_address = "sydneybloodworth33@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Biology',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2028',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CHEM 1211K", "CHEM 1212K", "BIOS 1207K", "BIOS 1208K", "BIOS 2300", "BIOS 2301", "CHEM 2311"], 
+    "names":["Principles of Chemistry I w/ Lab", "Principles of Chemistry II w/ Lab", "Intro to Biology I w/ Lab", "Intro to Biology II w/ Lab", "Ecology",
+             "Ecology Lab", "Organic Chemistry I"], 
+    "semester_taken":["1st", "2nd", "1st", "2nd","3rd","3rd","3rd"],
+    "skills":["Developed understanding of matter, energy, and chemical reactions based on the behavior of atoms and molecules.",
+              "Applied concepts in kinetics, chemical equilibrium, acid–base chemistry, electrochemistry, and thermodynamics to analyze and solve complex chemical problems.",
+              "Gained foundational experience in cell biology, genetics, ecology, and physiology, with hands-on laboratory skills in microscopy, data collection, and experimental analysis.",
+              "Built understanding of organismal biology, evolution, and ecology, while developing laboratory skills in dissection, experimental design, and data analysis.",
+              "Studied population dynamics, species interactions, and ecosystem processes, applying quantitative methods to analyze ecological data and environmental systems.",
+              "Conducted field and laboratory experiments measuring population dynamics, biodiversity, and environmental factors, with emphasis on data collection, statistical analysis, and scientific reporting.",
+              "Developed knowledge of structure, bonding, stereochemistry, and reaction mechanisms of organic molecules, applying problem-solving skills to predict and analyze chemical behavior."],
+    }
+experience_data = {
+    "Technician at Pleasant Hill Animal Hospital": (["- Assisted veterinarians with examinations, diagnostics, and surgical procedures, ensuring patient safety and comfort.","- Performed laboratory tests, sample collection, and imaging to support accurate diagnosis and treatment planning.","- Administered medications, vaccinations, and IV fluids while monitoring animal vitals and responses.", "- Educated pet owners on treatment plans, preventive care, and post-procedure instructions to improve outcomes."],"Images/vet.jpg"),
+    "Shadowing a Rheumatologist":(["- Observed patient consultations and diagnostic processes in a clinical rheumatology setting.","- Gained exposure to autoimmune and musculoskeletal disorders, including treatment approaches and patient management.","- Developed insight into physician–patient communication and professional medical practice."],"Images/shadow.jpeg"),
+
+}
+
+projects_data = {
+    "App for Lupus Symptom Management": "Created an interactive interface that tracks Lupus symptoms over time in order to help individuals better understand and manage their symptoms.",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 0,
+    "C": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"German": "🇩🇪",
+    "English": "🇬🇧",
+    "Korean":"🇰🇷"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "German": "Advanced Low",
+    "Korean": "Novice High",
+
+
+}
+leadership_data = { "Teacher at EyeLevel":(["- Leads and teaches classes K through 8"])
+}
+activity_data={ "Volleyball club":(["- Participates in competitive matches", "- Libero/Defense Specialist"])
+}
